11914ca6beb60b82db19ba7d667107676d399c5a
Yuri Marinho <ymarinho2025@gmail.com>
2026-01-26 13:49:51 -0300

    SellerDaoJDBC and DaoFactory

---
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -0,0 +1,34 @@
+package model.dao.impl;
+
+import java.util.List;
+
+import model.dao.SellerDao;
+import model.entities.Seller;
+
+public class SellerDaoJDBC implements SellerDao {
+
+    @Override
+    public void insert(Seller obj) {
+        // implementação JDBC
+    }
+
+    @Override
+    public void update(Seller obj) {
+        // implementação JDBC
+    }
+
+    @Override
+    public void deleteById(Integer id) {
+        // implementação JDBC
+    }
+
+    @Override
+    public Seller findById(Integer id) {
+        return null;
+    }
+
+    @Override
+    public List<Seller> findAll() {
+        return null;
+    }
+}
