450d1a5cf08cf447b3b27f5099bae9dc5946708a
Yuri Marinho <ymarinho2025@gmail.com>
2026-01-28 12:30:01 -0300

    Reutilizando a instanciação

--- src/application/Program.java
+++ src/application/Program.java
@@ -13,8 +13,9 @@ public class Program {
 
         SellerDao sellerDao = DaoFactory.createSellerDao();
 
+        System.out.println("=== TEST 1: seller findbyId ===");
         Seller seller = sellerDao.findById(3);
-        
+
         System.out.println(seller);
     }
 }
