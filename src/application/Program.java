c4675094f35ccf0c6105c84df4085f9d6b4a5e95
Yuri Marinho <ymarinho2025@gmail.com>
2026-01-28 13:33:47 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -31,10 +31,17 @@ public class Program {
             System.out.println(obj);
         }
 
-        System.out.println("=== TEST 4: seller insert===");
+        System.out.println("=== TEST 4: seller insert ===");
         Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
         sellerDao.insert(newSeller);
         System.out.println("Inserido! Novo id = " + newSeller.getId());
+
+
+        System.out.println("=== TEST 5: seller update ===");
+        seller = sellerDao.findById(1);
+        seller.setName("Martha Waine");
+        sellerDao.update(seller);
+        System.out.println("Atualização completa");
 
     }
 }
