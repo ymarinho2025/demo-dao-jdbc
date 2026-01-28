694d8109e5e08c7e957b5e827c8ba5c7a3a892b2
Yuri Marinho <ymarinho2025@gmail.com>
2026-01-28 13:25:40 -0300

    Insert implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -31,5 +31,10 @@ public class Program {
             System.out.println(obj);
         }
 
+        System.out.println("=== TEST 4: seller insert===");
+        Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
+        sellerDao.insert(newSeller);
+        System.out.println("Inserido! Novo id = " + newSeller.getId());
+
     }
 }
